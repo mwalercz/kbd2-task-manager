3c854533f3babd04ab43d7616a933040e72935c3
mwalercz <mwalerczuk@gmail.com>
2017-01-16 17:24:24 +0100

    added comments to web/pl-sql module

--- etap3/kbd2a01_3_web.sql
+++ etap3/kbd2a01_3_web.sql
@@ -1,4 +1,9 @@
 CREATE OR REPLACE PACKAGE tasks_rpt AS
+  /* html procedure renders projects form, where we can pick which report we want to see */
+  /* rpt procedure renders report about tasks in given project. Tasks are grouped by state */
+  /* links: http://ora1.elka.pw.edu.pl:8080/kbd/tasks_rpt.html */
+  /* http://ora1.elka.pw.edu.pl:8080/kbd/tasks_rpt.rpt?proj_id=1 */
+  /* proj_id examples: 1 or 2 */
   PROCEDURE html;
   PROCEDURE rpt (proj_id NUMBER DEFAULT 1);
 END tasks_rpt;
@@ -12,6 +17,7 @@ CREATE OR REPLACE PACKAGE BODY tasks_rpt AS
     END;
 
   PROCEDURE print_data (value VARCHAR2) IS
+    /* prints one task */
     BEGIN
       htp.div;
       htp.prn(value);
@@ -19,6 +25,7 @@ CREATE OR REPLACE PACKAGE BODY tasks_rpt AS
     END print_data;
 
   PROCEDURE print_new_header(header VARCHAR2) IS
+    /* prints state header */
     BEGIN
       htp.div(NULL, 'style="float:left;margin-left:40px;"');
       htp.header(4, header);
@@ -59,24 +66,24 @@ CREATE OR REPLACE PACKAGE BODY tasks_rpt AS
         WHERE STATES_IN_PROJECTS.PROJECT_ID = param_proj_id
         ORDER BY STATES.STATE_CODE, TASKS.TASK_NO;
 
-    v_task cur_task%ROWTYPE;
+    temp_task cur_task%ROWTYPE;
     temp_state_code STATES.STATE_CODE%TYPE;
     temp_project_name PROJECTS.NAME%TYPE;
 
     BEGIN
       SELECT NAME INTO temp_project_name FROM PROJECTS WHERE PROJECT_ID = proj_id;
       htp.header(2, 'Project ' || temp_project_name);
-      FOR v_task in cur_task(proj_id)
+      FOR temp_task in cur_task(proj_id)
       LOOP
         IF temp_state_code is NULL THEN
-          temp_state_code := v_task.STATE_CODE;
-          print_new_header(v_task.STATE_NAME);
-        ELSIF temp_state_code <> v_task.STATE_CODE THEN
+          temp_state_code := temp_task.STATE_CODE;
+          print_new_header(temp_task.STATE_NAME);
+        ELSIF temp_state_code <> temp_task.STATE_CODE THEN
           close_div();
-          temp_state_code := v_task.STATE_CODE;
-          print_new_header(v_task.STATE_NAME);
+          temp_state_code := temp_task.STATE_CODE;
+          print_new_header(temp_task.STATE_NAME);
         END IF;
-        print_data(v_task.TASK_NO || ' - ' || v_task.TASK_NAME);
+        print_data(temp_task.TASK_NO || ' - ' || temp_task.TASK_NAME);
       END LOOP;
     END rpt;
 
